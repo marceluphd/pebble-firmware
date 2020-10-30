ba32b4f26bf0e2ac6af392f04ce5e854f5b3d9b1
Xinxin Fan <xinxin@iotex.io>
2020-10-29 22:30:05 -0500

    Remove Pebble 2019 PoC

--- 2019poc/zephyr/cmake/toolchain/zephyr/generic.cmake
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-if (NOT ZEPHYR_SDK_INSTALL_DIR)
-  message(FATAL_ERROR "ZEPHYR_SDK_INSTALL_DIR must be set")
-endif()
-
-include(${ZEPHYR_BASE}/cmake/toolchain/zephyr/${SDK_MAJOR_MINOR}/generic.cmake)
